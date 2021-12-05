0b7e32d93172cc1758cc13084f6477eecb4e79fd
RalphCervantes <rafael.g.cervantes@gmail.com>
2021-12-05 12:19:02 -0800

    Small changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import headshot from '/home/me/Documents/Nextjs/nextjs-course/public/Headshot_2.jpg'
+
 function HomePage() {
-    return <h1>The Home Page</h1>
+    return(
+        <div>
+            <h1>The Home Page</h1>
+            <h3>This is just a test message</h3>
+            <img src='../public/Headshot_2.jpg' alt='Headshot'/>
+        </div>
+    ) 
 
 }
 export default HomePage;
